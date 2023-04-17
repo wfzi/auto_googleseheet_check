d9e8637d432282b57909439b2fd98dce7f52f014
wfzi <93904262+wfzi@users.noreply.github.com>
2023-04-17 14:06:04 -0500

    Update OAuth_function.py

--- OAuth_function.py
+++ OAuth_function.py
@@ -8,36 +8,49 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+import OAuth_function
 
-def get_values(spreadsheet_id: 'str', range_name: 'str', creds: 'Credentials') -> 'list[list[str]]':
-    """
-    Creates the batch_update the user has access to.
-    Load pre-authorized user credentials from the environment.
-    TODO(developer) - See https://developers.google.com/identity
-    for guides on implementing OAuth2 for the application.
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.metadata.readonly']
 
-    Parameters: spreadsheet_id: str
-                The id of the spreadsheet.
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = '1zM-9tdsbCMwqEdGILtiHE6WPUMCkpEk5kdKcYBAICA4'
+RANGE_NAME = 'OPT subscription tracking!A:G'
 
-                range_name: str
-                colume and row name of the cells you want to retrieve. example: A1:C2
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-                creds: Credentials
-                credentials of google api
+    #Get information from OPT subscription tracking sheets
+    values = OAuth_function.get_values(SPREADSHEET_ID, RANGE_NAME, creds)[1:]
     
-    Returns:    list[list[str]]
-                the value of the cells you retrived
-        """
-    try:
-        service = build('sheets', 'v4', credentials=creds)
+    #loop into each person
+    for row, person in enumerate(values):
+        #get name and day
+        month, day, year, name = person[0].split('-')
 
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=spreadsheet_id,
-                                    range=range_name).execute()
-        values = result.get('values', [])
-        
-        return values
-    except HttpError as err:
-        print(err)
-    
+        #add sheet id if miss
+        if len(person) <= 6 or person[6] == "No files found.":
+            cur_id = OAuth_function.get_sheet_id(name, creds)
+            OAuth_function.update_values(SPREADSHEET_ID,
+                  "OPT subscription tracking!G" + str(row + 2), "USER_ENTERED",
+                  [[cur_id]], creds)
+
+
+if __name__ == '__main__':
+    main()
