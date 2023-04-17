ef1a0c1209d388024302e398da531cf4f76aabb1
wfzi <93904262+wfzi@users.noreply.github.com>
2023-04-17 14:07:04 -0500

    Update OAuth_function.py

--- OAuth_function.py
+++ OAuth_function.py
@@ -8,49 +8,98 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-import OAuth_function
+def get_values(spreadsheet_id: 'str', range_name: 'str', creds: 'Credentials') -> 'list[list[str]]':
+    """
+    Get the value from a google sheets by specific sheets id, range and credentials
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.metadata.readonly']
+    Parameters: spreadsheet_id: str
+                The id of the spreadsheet.
 
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1zM-9tdsbCMwqEdGILtiHE6WPUMCkpEk5kdKcYBAICA4'
-RANGE_NAME = 'OPT subscription tracking!A:G'
+                range_name: str
+                Sheets name, colume and row name of the cells you want to retrieve. example: A1:C2
 
-def main():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    #Get information from OPT subscription tracking sheets
-    values = OAuth_function.get_values(SPREADSHEET_ID, RANGE_NAME, creds)[1:]
+                creds: Credentials
+                Credentials of google api
     
-    #loop into each person
-    for row, person in enumerate(values):
-        #get name and day
-        month, day, year, name = person[0].split('-')
+    Returns:    list[list[str]]
+                The value of the cells you retrived
+        """
+    try:
+        service = build('sheets', 'v4', credentials=creds)
 
-        #add sheet id if miss
-        if len(person) <= 6 or person[6] == "No files found.":
-            cur_id = OAuth_function.get_sheet_id(name, creds)
-            OAuth_function.update_values(SPREADSHEET_ID,
-                  "OPT subscription tracking!G" + str(row + 2), "USER_ENTERED",
-                  [[cur_id]], creds)
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=spreadsheet_id,
+                                    range=range_name).execute()
+        values = result.get('values', [])
+        
+        return values
+    except HttpError as err:
+        print(err)
 
 
-if __name__ == '__main__':
-    main()
+def get_sheet_id(name: 'str', creds: 'Credentials') -> 'str':
+    """
+    get the weekly report sheets id by name and credentials
+
+    Parameters: name: str
+                Name of the people whose weekly report sheets id we want to get.
+
+                creds: Credentials
+                Credentials of google api
+    
+    Returns:    str
+                The weekly report sheets id
+        """
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            q="'1w-41nhWBFJFjWGXTT4WOTfSbbN-GVbyx' in parents", fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+        
+        for item in items:
+            cur_name = item['name'].split('-')[0]
+            if cur_name == name:
+                return item['id']
+            
+        return 'No files found.'
+    except HttpError as err:
+        print(err)
+
+def update_values(spreadsheet_id: 'str', range_name: 'str', value_input_option: 'str', values: 'list[list[str]]', creds: 'Credentials'):
+    """
+    Write the content into google sheets
+
+    Parameters: spreadsheet_id: str
+                The id of the spreadsheet.
+
+                range_name: str
+                Sheets name, colume and row name of the cells you want to retrieve. example: A1:C2
+
+                value_input_option: str
+                How we want to input our value
+
+                values: The values we want to write in
+
+                creds: Credentials
+                Credentials of google api
+    
+    Returns:    list[list[str]]
+                The value of the cells you retrived
+        """
+    # pylint: disable=maybe-no-member
+    try:
+
+        service = build('sheets', 'v4', credentials=creds)
+        body = {
+            'values': values
+        }
+        result = service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id, range=range_name,
+            valueInputOption=value_input_option, body=body).execute()
+        print(f"{result.get('updatedCells')} cells updated.")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
